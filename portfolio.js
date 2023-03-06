e8349897978a8ddebbece7c597c38bde028f5bf5
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-06 18:39:42 -0300

    Add mobile menu

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,25 @@
-function showMenu() {
-  const elements = document.querySelectorAll('.navbar, .headline, .works, .about, .form');
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const close = document.querySelector('.close');
+
+
+hamburger.addEventListener('click', () => {
+  const elements = document.querySelectorAll('.logo, .hamburger, .headline, .works, .about, .form');
+
   elements.forEach((element) => {
     element.style.display = 'none';
   });
 
   mobileMenu.style.display = 'flex';
-  mobileMenu.style.width = '100%';
-  mobileMenu.style.height = '184px';
+  mobileMenu.style.width = '100vw';
+  mobileMenu.style.height = '100vh'
+});
 
-}
+close.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+  const elements = document.querySelectorAll('.logo, .hamburger, .headline, .works, .about, .form');
+
+  elements.forEach((element) => {
+    element.style.display = 'flex';
+  });
+});
