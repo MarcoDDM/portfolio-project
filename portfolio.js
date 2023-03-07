a439dc35b186a86cfc188ec5a440c59a5cfa8d35
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-03-06 22:18:13 -0300

    Fix linters errors

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const close = document.querySelector('.close');
+const portfolio = document.querySelector('.portfolio');
+const aboutMe = document.querySelector('.about-me');
+const contact = document.querySelector('.contact');
 
 hamburger.addEventListener('click', () => {
   const elements = document.querySelectorAll('.logo, .hamburger, .headline, .works, .about, .form');
@@ -23,12 +26,29 @@ close.addEventListener('click', () => {
   });
 });
 
-function closeMenu() {
+portfolio.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
   const elements = document.querySelectorAll('.logo, .hamburger, .headline, .works, .about, .form');
 
   elements.forEach((element) => {
     element.style.display = 'flex';
   });
+});
 
+aboutMe.addEventListener('click', () => {
   mobileMenu.style.display = 'none';
-}
+  const elements = document.querySelectorAll('.logo, .hamburger, .headline, .works, .about, .form');
+
+  elements.forEach((element) => {
+    element.style.display = 'flex';
+  });
+});
+
+contact.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+  const elements = document.querySelectorAll('.logo, .hamburger, .headline, .works, .about, .form');
+
+  elements.forEach((element) => {
+    element.style.display = 'flex';
+  });
+});
